5c5df4770c3eeb5ec9914245a92f217297b713bd
Lucas Henrique Paes de Carvalho <lukas.paes18@gmail.com>
2021-09-21 00:22:07 -0300

    update

--- app/Controllers/Http/InviteController.js
+++ app/Controllers/Http/InviteController.js
@@ -1,34 +1,26 @@
-'use strict'
+'use strict';
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-const Invite = use('App/Models/Invite')
+const Invite = use('App/Models/Invite');
 
 /**
  * Resourceful controller for interacting with invites
  */
 class InviteController {
-  /**
-   * Create/save a new invite.
-   * POST invites
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response, auth }) {
-    const invites = request.input('invites')
+  async store({ request, response, auth }) {
+    const invites = request.input('invites');
 
-    const data = invites.map(email => ({
+    const data = invites.map((email) => ({
       email,
       user_id: auth.user.id,
-      provider_id: request.provider.id
-    }))
+      provider_id: request.provider.id,
+    }));
 
-    await Invite.createMany(data)
+    await Invite.createMany(data);
   }
 }
 
-module.exports = InviteController
+module.exports = InviteController;
